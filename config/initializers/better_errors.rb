92ecc6476171dde6d6dcd209c751464ec5cbbe13
kataoka-01 <44348941+kataoka-01@users.noreply.github.com>
2018-10-23 12:29:42 +0900

    Add files via upload

---
+++ config/initializers/better_errors.rb
@@ -0,0 +1,11 @@
+if defined?(Footnotes) && Rails.env.development?
+  Footnotes.setup do |f|
+    Footnotes.enabled = true
+    f.prefix = 'x-mine://open?file=%s&line=%d'
+    f.lock_top_right = true
+  end
+end
+
+if defined?(BetterErrors) && Rails.env.development?
+  BetterErrors.editor = 'x-mine://open?file=%{file}&line=%{line}'
+end
